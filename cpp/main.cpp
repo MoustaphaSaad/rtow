54eadd6ebf5e58085db568655aefa2b67b20d1c1
Mostafa Saad <moustapha.saad.abdelhamed@gmail.com>
2022-11-04 19:28:03 +0200

    chapter 8 cpp impl

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -41,11 +41,16 @@ double hit_sphere(const point3& center, double radius, const ray& r)
 	}
 }
 
-color ray_color(const ray& r, const hittable& world)
+color ray_color(const ray& r, const hittable& world, int depth)
 {
 	hit_record rec;
-	if (world.hit(r, 0, infinity, rec)) {
-		return 0.5 * (rec.normal + color{1, 1, 1});
+
+	if (depth <= 0)
+		return color{};
+
+	if (world.hit(r, 0.001, infinity, rec)) {
+		auto target = rec.p + rec.normal + random_unit_vector();
+		return 0.5 * ray_color(ray{rec.p, target - rec.p}, world, depth - 1);
 	}
 	auto unit_direction = unit_vector(r.direction());
 	auto t = 0.5 * (unit_direction.y() + 1.0);
@@ -62,6 +67,7 @@ int main()
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
 	const int samples_per_pixel = 100;
 	const size_t rays_count = image_width * image_height * samples_per_pixel;
+	const int max_depth = 50;
 
 	// World
 	hittable_list world;
@@ -89,7 +95,7 @@ int main()
 				auto u = (i + random_double()) / (image_width - 1);
 				auto v = (j + random_double()) / (image_height - 1);
 				auto r = cam.get_ray(u, v);
-				pixel_color += ray_color(r, world);
+				pixel_color += ray_color(r, world, max_depth);
 			}
 			auto end = std::chrono::high_resolution_clock::now();
 			pixel_only += end - start;
--- cpp/vec3.h
+++ cpp/vec3.h
@@ -55,6 +55,16 @@ public:
 		return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
 	}
 
+	inline static vec3 random()
+	{
+		return vec3{random_double(), random_double(), random_double()};
+	}
+
+	inline static vec3 random(double min, double max)
+	{
+		return vec3{random_double(min, max), random_double(min, max), random_double(min, max)};
+	}
+
 public:
 	double e[3];
 };
@@ -118,4 +128,29 @@ inline vec3 cross(const vec3& u, const vec3& v)
 inline vec3 unit_vector(vec3 v)
 {
 	return v / v.length();
+}
+
+vec3 random_in_unit_sphere()
+{
+	while (true)
+	{
+		auto p = vec3::random(-1, 1);
+		if (p.length_squared() >= 1) continue;
+		return p;
+	}
+}
+
+vec3 random_unit_vector()
+{
+	return unit_vector(random_in_unit_sphere());
+}
+
+// old ray tracing book
+vec3 random_in_hemisphere(const vec3& normal)
+{
+	auto in_unit_sphere = random_in_unit_sphere();
+	if (dot(in_unit_sphere, normal) > 0.0)
+		return in_unit_sphere;
+	else
+		return -in_unit_sphere;
 }
