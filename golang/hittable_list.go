2478d77e736e8a0b094bd8f582300b79fc39ed5b
Mostafa Saad <moustapha.saad.abdelhamed@gmail.com>
2023-04-07 15:53:39 +0200

    go generate ispc code

--- golang/hittable_list.go
+++ golang/hittable_list.go
@@ -1,5 +1,7 @@
 package main
 
+//go:generate ispc -g --target=sse2,sse4,avx1,avx2 spheres_hit.ispc -o ./build/spheres_hit.o -h ./build/spheres_hit.h
+
 type HittableList struct {
 	materials []Material
 	spheres   []Sphere
