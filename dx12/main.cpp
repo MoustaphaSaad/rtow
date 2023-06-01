f8bd68242ac9bb41c4c9582482bb5c2c49d3f0c0
Mostafa Saad <moustapha.saad.abdelhamed@gmail.com>
2023-06-01 21:28:04 +0300

    init d3d12 device

--- dx12/main.cpp
+++ dx12/main.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <d3d12.h>
+#include <dxgi1_6.h>
+
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
 #include <Windows.h>
@@ -9,10 +12,21 @@ const char* WINDOW_CLASS = "rtow_window_class";
 
 constexpr int WINDOW_DEFAULT_WIDTH = 1024;
 constexpr int WINDOW_DEFAULT_HEIGHT = WINDOW_DEFAULT_WIDTH / (16.0 / 9.0);
+constexpr bool USE_WARP_DEVICE = false;
 
 void check_handle(HANDLE h, const char* msg)
 {
 	if (h == INVALID_HANDLE_VALUE)
+	{
+		DWORD error = GetLastError();
+		fprintf(stderr, "%s, Error: %d\n", msg, error);
+		exit(error);
+	}
+}
+
+void check_result(HRESULT h, const char* msg)
+{
+	if (FAILED(h))
 	{
 		DWORD error = GetLastError();
 		fprintf(stderr, "%s, Error: %d\n", msg, error);
@@ -27,6 +41,111 @@ struct Window
 	HWND handle;
 	HDC hdc;
 };
+
+struct Renderer
+{
+	IDXGIFactory4* factory;
+	ID3D12Device* device;
+};
+
+Renderer renderer;
+
+IDXGIAdapter1* get_hardware_adapter(IDXGIFactory1* factory, bool request_high_performance_adapter)
+{
+	IDXGIAdapter1* adapter = nullptr;
+	IDXGIFactory6* factory6 = nullptr;
+	if (SUCCEEDED(factory->QueryInterface(IID_PPV_ARGS(&factory6))))
+	{
+		for (
+			UINT adapter_index = 0;
+			SUCCEEDED(factory6->EnumAdapterByGpuPreference(
+				adapter_index,
+				request_high_performance_adapter ? DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE : DXGI_GPU_PREFERENCE_UNSPECIFIED,
+				IID_PPV_ARGS(&adapter)));
+			++adapter_index)
+		{
+			DXGI_ADAPTER_DESC1 desc;
+			adapter->GetDesc1(&desc);
+
+			if (desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE)
+			{
+				continue;
+			}
+
+			if (SUCCEEDED(D3D12CreateDevice(adapter, D3D_FEATURE_LEVEL_11_0, _uuidof(ID3D12Device), nullptr)))
+			{
+				break;
+			}
+		}
+	}
+
+	if (adapter == nullptr)
+	{
+		for (UINT adapter_index = 0; SUCCEEDED(factory->EnumAdapters1(adapter_index, &adapter)); ++adapter_index)
+		{
+			DXGI_ADAPTER_DESC1 desc;
+			adapter->GetDesc1(&desc);
+
+			if (desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE)
+			{
+				continue;
+			}
+
+			if (SUCCEEDED(D3D12CreateDevice(adapter, D3D_FEATURE_LEVEL_11_0, _uuidof(ID3D12Device), nullptr)))
+			{
+				break;
+			}
+		}
+	}
+
+	return adapter;
+}
+
+Renderer renderer_new()
+{
+	Renderer self{};
+
+	UINT dxgiFactoryFlags = 0;
+
+	#if defined(_DEBUG)
+	{
+		ID3D12Debug* debugController;
+		auto res = D3D12GetDebugInterface(IID_PPV_ARGS(&debugController));
+		check_result(res, "D3D12GetDebugInterface failed");
+
+		debugController->EnableDebugLayer();
+		dxgiFactoryFlags |= DXGI_CREATE_FACTORY_DEBUG;
+		debugController->Release();
+	}
+	#endif
+
+	auto res = CreateDXGIFactory2(dxgiFactoryFlags, IID_PPV_ARGS(&self.factory));
+	check_result(res, "CreateDXGIFactory2 failed");
+
+	if (USE_WARP_DEVICE)
+	{
+		IDXGIAdapter* warpAdapter;
+		auto res = self.factory->EnumWarpAdapter(IID_PPV_ARGS(&warpAdapter));
+		check_result(res, "EnumWarpAdapter failed");
+
+		res = D3D12CreateDevice(warpAdapter, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&self.device));
+		check_result(res, "D3D12CreateDevice failed");
+	}
+	else
+	{
+		IDXGIAdapter1* hardwareAdapter = get_hardware_adapter(self.factory, true);
+		res = D3D12CreateDevice(hardwareAdapter, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&self.device));
+		check_result(res, "D3D12CreateDevice failed");
+	}
+
+	return self;
+}
+
+void renderer_free(Renderer& self)
+{
+	self.device->Release();
+	self.factory->Release();
+}
 
 LRESULT CALLBACK _window_callback(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 {
@@ -102,6 +221,8 @@ void window_free(Window& self)
 
 int main(int argc, char** argv)
 {
+	renderer = renderer_new();
+
 	window_register_class(WINDOW_CLASS);
 	auto window = window_new(WINDOW_DEFAULT_WIDTH, WINDOW_DEFAULT_HEIGHT, "Ray Tracing in One Weekend");
 
@@ -121,5 +242,6 @@ int main(int argc, char** argv)
 	}
 
 	window_free(window);
+	renderer_free(renderer);
 	return 0;
 }
